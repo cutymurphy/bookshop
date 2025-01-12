8ac92636284cab6b3dd3b32bf9d0a6c0d3512073
Darya Sushkova <dasha.030105@gmail.com>
2025-01-12 23:35:03 +0300

    fix searching

--- src/assets/components/HeaderForm/HeaderForm.tsx
+++ src/assets/components/HeaderForm/HeaderForm.tsx
@@ -1,18 +1,36 @@
 import React, { FC, useEffect, useState } from "react";
-import styles from './HeaderForm.module.scss'
+import styles from "./HeaderForm.module.scss";
 import { IHeaderForm } from "./types";
+import { useLocation, useNavigate } from "react-router-dom";
+import { EPath } from "../../../AppPathes.ts";
 
 const HeaderForm: FC<IHeaderForm> = ({
     setSearchInput,
 }) => {
     const [inputValue, setInputValue] = useState<string>("");
 
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname !== EPath.main) {
+            setInputValue("");
+            setSearchInput("");
+        }
+    }, [location.pathname, setSearchInput]);
+
+    const clickSearch = () => {
+        if (location.pathname !== EPath.main) {
+            navigate(EPath.main);
+        }
+    };
+
     const handleKeyDown = (e: React.KeyboardEvent) => {
         if (e.key === "Enter") {
             e.preventDefault();
             setSearchInput(inputValue);
         }
-    }
+    };
 
     const setInput = () => {
         setSearchInput(inputValue);
@@ -26,15 +44,15 @@ const HeaderForm: FC<IHeaderForm> = ({
 
     return (
         <div className={styles.search}>
-            <form className={styles.headerForm}>
-                {/* TO-DO: сделать так, чтобы поиск работал на любой странице */}
+            <form className={styles.headerForm} onClick={clickSearch}>
                 <input
+                    value={inputValue}
                     type="search"
                     placeholder="Search..."
                     autoFocus
                     required
                     onChange={(e) => setInputValue(String(e.target.value))}
-                    onKeyDown={(e) => handleKeyDown(e)}
+                    onKeyDown={handleKeyDown}
                 />
                 <button
                     id="search"
@@ -45,7 +63,7 @@ const HeaderForm: FC<IHeaderForm> = ({
                 </button>
             </form>
         </div>
-    )
-}
+    );
+};
 
 export default HeaderForm;
