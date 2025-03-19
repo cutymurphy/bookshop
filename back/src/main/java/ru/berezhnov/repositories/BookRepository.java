e7df0e91cccc6ec8774901d025b1771f26e9569d
Владислав Бережнов <berezhnovvladislav2012@gmail.com>
2025-03-19 20:04:06 +0300

    add repository annotation

--- back/src/main/java/ru/berezhnov/repositories/BookRepository.java
+++ back/src/main/java/ru/berezhnov/repositories/BookRepository.java
@@ -1,7 +1,9 @@
 package ru.berezhnov.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.berezhnov.models.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, String> {
 }
--- back/src/main/java/ru/berezhnov/repositories/UserRepository.java
+++ back/src/main/java/ru/berezhnov/repositories/UserRepository.java
@@ -1,10 +1,12 @@
 package ru.berezhnov.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.berezhnov.models.UserWithCart;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserWithCart, String> {
     Optional<UserWithCart> findByEmail(String email);
 }
