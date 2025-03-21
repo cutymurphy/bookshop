3f268ed7864667556f7160c2ac33e40a51612e36
Владислав Бережнов <berezhnovvladislav2012@gmail.com>
2025-03-22 00:25:26 +0300

    add updateUser, loadUserAndCart, loadUserAndCart, deleteUser methods and move modelMapper methods here

--- back/src/main/java/ru/berezhnov/controller/UserController.java
+++ back/src/main/java/ru/berezhnov/controller/UserController.java
@@ -4,47 +4,86 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.berezhnov.dto.AuthenticationRequest;
-import ru.berezhnov.dto.AuthenticationResponse;
-import ru.berezhnov.dto.RegisterRequest;
+import ru.berezhnov.dto.*;
+import ru.berezhnov.models.UserBook;
 import ru.berezhnov.models.UserWithCart;
 import ru.berezhnov.services.AuthenticationService;
 import ru.berezhnov.services.UserService;
+import ru.berezhnov.util.AppException;
+import ru.berezhnov.util.EmailExtractor;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api")
 public class UserController {
 
     private final AuthenticationService authenticationService;
     private final ModelMapper modelMapper;
     private final UserService userService;
+    private final EmailExtractor emailExtractor;
 
     @Autowired
-    public UserController(AuthenticationService authenticationService, ModelMapper modelMapper, UserService userService) {
+    public UserController(AuthenticationService authenticationService, ModelMapper modelMapper, UserService userService, EmailExtractor emailExtractor, EmailExtractor emailExtractor1) {
         this.authenticationService = authenticationService;
         this.modelMapper = modelMapper;
         this.userService = userService;
+        this.emailExtractor = emailExtractor1;
     }
 
-    @PostMapping("/register")
+    @PostMapping("/auth/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authenticationService.register(modelMapper.map(request, UserWithCart.class)));
     }
 
-    @PostMapping("/login")
+    @PostMapping("/auth/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(authenticationService.login(request));
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable String id, @RequestBody UserWithCart user) {
-        return null;
+    @PutMapping("/user/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable UUID id, @RequestBody UserDTO user) {
+        user.setId(id);
+        userService.update(convertToUser(user));
+        return ResponseEntity.ok().build();
     }
 
-    @GetMapping
-    public ResponseEntity<List<UserWithCart>> fetchUsers() {
-        return ResponseEntity.ok(userService.findAll());
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDTO>> fetchUsers() {
+        return ResponseEntity.ok(userService.findAll().stream().map(this::convertToUserDTO).toList());
+    }
+
+    @GetMapping("/user/load")
+    public ResponseEntity<UserCartResponse> loadUserAndCart(@RequestHeader("Authorization") String authHeader) {
+        return ResponseEntity.ok(convertToUserCartResponse(userService.findByEmail(emailExtractor.getUserFromHeader(authHeader)
+                        .getEmail()).orElseThrow(() -> new AppException("Пользователь не найден"))));
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<UserDTO> getUser(@RequestHeader("Authorization") String authHeader) {
+        return ResponseEntity.ok(convertToUserDTO(userService.findByEmail(emailExtractor.getUserFromHeader(authHeader)
+                .getEmail()).orElseThrow(() -> new AppException("Пользователь не найден"))));
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable UUID id) {
+        userService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    private UserCartResponse convertToUserCartResponse(UserWithCart user) {
+        UserCartResponse userCartResponse = modelMapper.map(user, UserCartResponse.class);
+        userCartResponse.setCartBooks(user.getUserBooks().stream().map(UserBook::getBook)
+                .map(b -> modelMapper.map(b, BookDTO.class)).toList());
+        return userCartResponse;
+    }
+
+    private UserDTO convertToUserDTO(UserWithCart user) {
+        return modelMapper.map(user, UserDTO.class);
+    }
+
+    private UserWithCart convertToUser(UserDTO userDTO) {
+        return modelMapper.map(userDTO, UserWithCart.class);
     }
 }
