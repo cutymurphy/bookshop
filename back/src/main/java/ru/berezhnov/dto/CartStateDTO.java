d1616ba90cd4835e25ffb645b3ce4e072dccd881
Владислав Бережнов <berezhnovvladislav2012@gmail.com>
2025-03-22 22:55:35 +0300

    rework class

--- back/src/main/java/ru/berezhnov/dto/CartStateDTO.java
+++ back/src/main/java/ru/berezhnov/dto/CartStateDTO.java
@@ -3,22 +3,40 @@ package ru.berezhnov.dto;
 import java.util.UUID;
 
 public class CartStateDTO {
-    private UUID bookInfo;
-    private Integer count;
+    private UUID id;
+    private UUID bookId;
+    private Integer bookCount;
+    private UUID orderId;
 
-    public UUID getBookInfo() {
-        return bookInfo;
+    public UUID getId() {
+        return id;
     }
 
-    public void setBookInfo(UUID bookInfo) {
-        this.bookInfo = bookInfo;
+    public void setId(UUID id) {
+        this.id = id;
     }
 
-    public Integer getCount() {
-        return count;
+    public UUID getBookId() {
+        return bookId;
     }
 
-    public void setCount(Integer count) {
-        this.count = count;
+    public void setBookId(UUID bookId) {
+        this.bookId = bookId;
+    }
+
+    public Integer getBookCount() {
+        return bookCount;
+    }
+
+    public void setBookCount(Integer bookCount) {
+        this.bookCount = bookCount;
+    }
+
+    public UUID getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(UUID orderId) {
+        this.orderId = orderId;
     }
 }
